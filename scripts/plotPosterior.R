d82de96c4b4b01010a33672dffac2fa9c7a00e9e
Guilherme Azevedo <68890308+GHFAzevedo@users.noreply.github.com>
2025-03-01 15:42:15 -0500

    Update plotPosterior.R

--- scripts/plotPosterior.R
+++ scripts/plotPosterior.R
@@ -6,6 +6,8 @@ library(coda)
 # file names
 fp = "../" 
 traceFile = paste(fp, "outputs/Pho_PolymorSSE.model.log", sep="")
+output_file = paste(fp, "outputs/Pho_PolymorSSE.Posterior", sep="")
+
 variables=c("extinction_eic",
             "extinction_key",
             "extinction_nig",
@@ -15,6 +17,11 @@ traceModel <- readTrace(traceFile, burnin = 0.1)
 
 plotExtinction <- plotTrace(trace = traceModel, vars = variables)
 plotExtinction
+ggsave(filename = paste0(output_file, ".extinction.pdf"))
+ggsave(filename = paste0(output_file, ".extinction.png"))
 
 plotTransition <- plotTrace(trace = traceModel, match = "em")
 plotTransition
+ggsave(filename = paste0(output_file, ".transition.pdf"))
+ggsave(filename = paste0(output_file, ".transition.png"))
+
